77a711dff8cc37e33d8297f71840b3c1af4803ae
B Mohnish Karthikeyan <94187409+MohnishKarthikeyan@users.noreply.github.com>
2023-01-06 22:55:04 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// For the mobile-phone layout (navbar {side bar})
+
 var navLinks = document.getElementById("navLinks");
 
 function showmenu() {
@@ -7,3 +9,37 @@ function showmenu() {
 function hidemenu() {
   navLinks.style.right = "-200px";
 }
+
+// For the scroll Menu active part code:
+
+$(document).ready(function (e) {
+  $("a[href*=#]").bind("click", function () {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(target).offset().top,
+        },
+        600,
+        function () {
+          location.hash = target;
+        }
+      );
+
+    return false;
+  });
+});
+
+$(window)
+  .scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+    $(".page-section").each(function (i) {
+      if ($(this).position().top <= scrollDistance) {
+        $(".navigation a.active").removeClass("active");
+        $(".navigation a").eq(i).addClass("active");
+      }
+    });
+  })
+  .scroll();
