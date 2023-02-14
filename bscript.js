8efabc555679cf98201b095bc5df1ea3ee9230c8
Kalyanasundaram V <112867527+Kalyanasundaram12@users.noreply.github.com>
2023-02-14 20:04:31 +0530

    updated js

--- bscript.js
+++ bscript.js
@@ -60,3 +60,18 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
+  function myFunction4() {
+    var dots = document.getElementById("dots4");
+    var moreText = document.getElementById("more4");
+    var btnText = document.getElementById("myBtn4");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
