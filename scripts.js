59e3cd97151a7232480b218947c5ce25f34a68e4
Kalyanasundaram V <112867527+Kalyanasundaram12@users.noreply.github.com>
2023-05-18 19:34:06 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -42,3 +42,83 @@ $(window)
     });
   })
   .scroll();
+
+
+  const form = document.getElementById("contact-form");
+const nameField = form.elements["name"];
+const emailField = form.elements["email"];
+const messageField = form.elements["message"];
+const submitButton = form.elements["submit"];
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  if (validateName() && validateEmail() && validateMessage()) {
+    submitButton.disabled = true;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", form.action);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          alert(xhr.responseText);
+          form.reset();
+          submitButton.disabled = false;
+        } else {
+          alert("There was an error sending your message. Please try again.");
+          submitButton.disabled = false;
+        }
+      }
+    };
+    xhr.send(encodeFormData());
+  }
+});
+
+function validateName() {
+  const nameValue = nameField.value.trim();
+  if (nameValue === "") {
+    alert("Please enter your name.");
+    nameField.focus();
+    return false;
+  }
+  return true;
+}
+
+function validateEmail() {
+  const emailValue = emailField.value.trim();
+  if (emailValue === "") {
+    alert("Please enter your email address.");
+    emailField.focus();
+    return false;
+  } else if (!isValidEmail(emailValue)) {
+    alert("Please enter a valid email address.");
+    emailField.focus();
+    return false;
+  }
+  return true;
+}
+
+function validateMessage() {
+  const messageValue = messageField.value.trim();
+  if (messageValue === "") {
+    alert("Please enter your message.");
+    messageField.focus();
+    return false;
+  }
+  return true;
+}
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function encodeFormData() {
+  const formData = new FormData(form);
+  const encodedData = [];
+  for (const pair of formData.entries()) {
+    encodedData.push(encodeURIComponent(pair[0]) + "=" + encodeURIComponent(pair[1]));
+  }
+  return encodedData.join("&");
+}
